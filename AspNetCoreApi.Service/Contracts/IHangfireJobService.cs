50c652693cde09f5e83850c1f55606716be7a4f8
Ardit <mezini.ardit@gmail.com>
2020-03-15 13:20:29 +0100

    Added IHangfireJobService

---
+++ AspNetCoreApi.Service/Contracts/IHangfireJobService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AspNetCoreApi.Service.Contracts
+{
+    public interface IHangfireJobService
+    {
+        Task ProcessFireAndForgetJobs<T>(T content);
+        void ProcessDelayedJob<T>(T content, TimeSpan delay);
+        void ProcessRecurringJob<T>(string recurringJobId, T content, string cronExpression, TimeZoneInfo timeZone = null, string queue = "default");
+    }
+}
