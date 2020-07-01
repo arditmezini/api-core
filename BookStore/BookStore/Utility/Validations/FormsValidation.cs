967cef7583f6919e32a7079b85959216f4429ad6
Ardit <mezini.ardit@gmail.com>
2020-07-01 09:35:41 +0200

    Updated nuget packages

---
+++ BookStore/BookStore/Utility/Validations/FormsValidation.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+
+namespace BookStore.Utility.Validations
+{
+    public static class FormsValidation
+    {
+        public static string IsEmptyString(this string property)
+        {
+            return string.IsNullOrWhiteSpace(property) ?
+                string.Empty : $"{nameof(property)} is required.";
+        }
+
+        public static string IsEmail(this string email)
+        {
+            var addr = new MailAddress($"{email}");
+            return addr.Address == $"{email}" ?
+                string.Empty : "Email address is not valid.";
+        }
+    }
+}
