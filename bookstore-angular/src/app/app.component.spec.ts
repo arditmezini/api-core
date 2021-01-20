e61068bb2f668d9c58c8d590f8ecd5ed2925f0dd
Ardit <mezini.ardit@gmail.com>
2021-01-20 10:07:29 +0100

    Solution structure update and nuget update

--- bookstore-angular/src/app/app.component.spec.ts
+++ bookstore-angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular'`, () => {
+  it(`should have as title 'bookstore-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular');
+    expect(app.title).toEqual('bookstore-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bookstore-angular app is running!');
   });
 });
